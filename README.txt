35c6522587622f3f7606dcc458e7056a779377d0
OwainCharlon <ow.charlon@gmail.com>
2020-05-09 00:58:33 +0200

    password hashing is done

--- README.txt
+++ README.txt
@@ -5,6 +5,7 @@ Installer flask: pip install flask
 - flask-sqlalchemy 2.4.1
 - mysqlclient 1.4.6
 - pymysql
+- passlib 1.7.2
 Mettre ses templates dans un dossier nommé templates (respecter l'architecture du framework).
 Mettre ses fichiers static (scripts js, feuille de style css, images...) dans un fichier static.
 Appeller un fichier dans le dossier static :
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask import Flask, redirect, url_for, render_template, request, session, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
+
+from passlib.hash import sha256_crypt #Use for password hashing
 
 from models import *
 import config
@@ -16,10 +19,6 @@ def homepage():
     return render_template("index.html")
 
 
-@app.route("/signin")
-def signin():
-    return render_template("signin.html")
-
 @app.route("/search")
 def search():
     return render_template("search.html")
@@ -27,21 +26,45 @@ def search():
 
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
+    
     if request.method == "POST":
         email = request.form["inputEmail"]
         username = request.form["inputUsername"]
         password = request.form["inputPassword"]
         
-        usr = User(username, password, email)
-        db.session.add(usr)
-        db.session.commit() #Use everytime you make a change into your db
-        return redirect(url_for("signin"))
+        if db.session.query(User).filter(User.username==username).first() or db.session.query(User).filter(User.email==email).first():
+            return jsonify('user already exists')
+        
+        else:
+            db.session.add(User(username, sha256_crypt.encrypt(password), email))
+            db.session.commit() #Use everytime you make a change into your db.
+            return redirect(url_for("signin"))
+        
     else:
         return render_template("signup.html")
+
+
+@app.route("/signin", methods=["POST", "GET"])
+def signin():
+    
+    if request.method == "POST":
+        email = request.form['inputEmail']
+        password = request.form['inputPassword']
+
+        usr = db.session.query(User).filter(User.email==email).first()
+        
+        if usr and sha256_crypt.verify(password, usr.password):
+            return jsonify({'message': 'Password is correct'}) 
+        
+        else:
+            return jsonify({'error': 'User or password are incorrect'})
+
+    else:
+        return render_template("signin.html")
 
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    db.create_all() #ORM initialisation.
+    db.create_all() #DB création with ORM and models.
