2869244b961f157e46038d3670b20fa7c9a068f8
Ynovbanj0 <paul.bagnis@ynov.com>
2020-05-12 12:01:57 +0200

    on finalise la page favoris pour faire fermer son claque merde a Owain

--- static/js/favorites.js
+++ static/js/favorites.js
@@ -1,1 +1,13 @@
-$('.listFav').append('<div id=maDiv class=classe>Coucou</div>');
+function toggleLikeFav() {
+    $(".like").toggleClass("liked");
+    $(".like").toggleClass("notLiked");
+}
+
+
+// Récupérer les ID
+// Faire un fetch des id et stocker URL et description et autre
+// boucler pour append chaque div
+
+
+
+//$('.listFav').append('<div class="favItem" class=classe>Coucou</div>');
