bd127aa057f2155cc415268e623e7872346db1b8
Ynovbanj0 <paul.bagnis@ynov.com>
2020-05-19 11:13:17 +0200

    test owain favoris$

--- static/js/favorites.js
+++ static/js/favorites.js
@@ -1,25 +1,60 @@
 var ids = new Array();
 ids.push($('.favItem'));
+var idLength = ids[0].length;
+// console.log(i);
 //console.log(ids);
-
-ids[0].each(function(index) {
-    //console.log(ids[0][index].id);
-    var apiEndpointBaseURL = "https://api.harvardartmuseums.org/object";
-    var queryString = $.param({
-        apikey: "b6782a10-8def-11ea-877a-6df674fda82b"
-    });
-    $.getJSON(apiEndpointBaseURL + "/" + ids[0][index].id + "?" + queryString, function(data) {
-        //console.log(data);
-        var id = '\#' + data.objectid;
-        element = $(id);
-        console.log(element.children());
-        element.children()[0].style.backgroundImage = data.images[0].baseimageurl;
-        //.css("background-image", "url(\"" + data.images[0].baseimageurl + "\"\)");
-        var authorTitle = (data.people) ? (data.title + " - " + data.people[0].name) : (data.title + " - Unknown Artist");
-        element.children()[1].children[0]
-            //.html(authorTitle);
-        var descHis = (data.labeltext) ? (data.labeltext) : ("Aucune description disponible. Déso !");
-        element.children()[1].children[1]
-            //.html(descHis);
-    });
+var favBackgrd = new Array();
+var apiEndpointBaseURL = "https://api.harvardartmuseums.org/object";
+var queryString = $.param({
+    apikey: "b6782a10-8def-11ea-877a-6df674fda82b"
 });
+
+
+var y = 0;
+while (y < idLength) {
+    $.getJSON(apiEndpointBaseURL + "/" + ids[0][y].id + "?" + queryString, function(data) {
+        //console.log(data);
+        var backgrd = 'url(\'' + data.images[0].baseimageurl + '\');';
+        favBackgrd.push(backgrd);
+        //console.log(backgrd);
+    });
+    y++;
+}
+
+console.log(favBackgrd);
+
+var z = 0;
+while (z < idLength) {
+    var element = ids[0][z];
+    //console.log($(element));
+    var elementChild = $(element).children();
+    console.log(z);
+    //console.log(backgrd);
+    $(elementChild[0]).css("background-image", favBackgrd[z]);
+    //console.log($(elementChild[0]));
+    z++;
+}
+
+
+
+// ids[0].each(function(index) {
+//     //console.log(ids[0][index].id);
+//     var apiEndpointBaseURL = "https://api.harvardartmuseums.org/object";
+//     var queryString = $.param({
+//         apikey: "b6782a10-8def-11ea-877a-6df674fda82b"
+//     });
+//     $.getJSON(apiEndpointBaseURL + "/" + ids[0][index].id + "?" + queryString, function(data) {
+//         console.log(data);
+//         // var id = '\#' + data.objectid;
+//         // element = $(id);
+//         //console.log(ids[index].children);
+//         // element.children()[0].style.backgroundImage = data.images[0].baseimageurl;
+//         // //.css("background-image", "url(\"" + data.images[0].baseimageurl + "\"\)");
+//         // var authorTitle = (data.people) ? (data.title + " - " + data.people[0].name) : (data.title + " - Unknown Artist");
+//         // element.children()[1].children[0]
+//         //     //.html(authorTitle);
+//         // var descHis = (data.labeltext) ? (data.labeltext) : ("Aucune description disponible. Déso !");
+//         // element.children()[1].children[1]
+//         //     //.html(descHis);
+//     });
+// });
