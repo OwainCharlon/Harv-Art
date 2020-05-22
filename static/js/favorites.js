c7853926debe505f9f3cb97929c19887000850bd
Ynovbanj0 <paul.bagnis@ynov.com>
2020-05-22 14:44:30 +0200

    on refile le bb a Owain

--- static/js/favorites.js
+++ static/js/favorites.js
@@ -2,40 +2,67 @@ var ids = new Array();
 ids.push($('.favItem'));
 var idLength = ids[0].length;
 // console.log(i);
-//console.log(ids);
+console.log(ids);
 var favBackgrd = new Array();
 var apiEndpointBaseURL = "https://api.harvardartmuseums.org/object";
 var queryString = $.param({
     apikey: "b6782a10-8def-11ea-877a-6df674fda82b"
 });
 
+// var y = 0;
+// while (y < idLength) {
+//     $.getJSON(apiEndpointBaseURL + "/" + ids[0][y].id + "?" + queryString, function(data) {
+//         //console.log(data);
+//         var backgrd = "url(\"" + data.images[0].baseimageurl + "\");";
+//         favBackgrd.push(backgrd);
+//         //console.log(backgrd);
+//     });
+//     y++;
+// }
 
-var y = 0;
-while (y < idLength) {
-    $.getJSON(apiEndpointBaseURL + "/" + ids[0][y].id + "?" + queryString, function(data) {
-        //console.log(data);
-        var backgrd = 'url(\'' + data.images[0].baseimageurl + '\');';
-        favBackgrd.push(backgrd);
-        //console.log(backgrd);
+// console.log(favBackgrd);
+
+// var z = 0;
+// while (z < idLength) {
+//     var element = ids[0][z];
+//     //console.log($(element));
+//     var elementChild = $(element).children();
+//     console.log(z);
+//     console.log(favBackgrd[z]);
+//     $(elementChild[0])[0].style.backgroundImage = favBackgrd[z];
+//     console.log($(elementChild[0])[0].style.backgroundImage);
+//     z++;
+// }
+
+
+function getBkgnd(ids, favBackgrd) {
+    return new Promise(function(resolve, reject) {
+        var y = 0;
+        while (y < idLength) {
+            $.getJSON(apiEndpointBaseURL + "/" + ids[0][y].id + "?" + queryString, function(data) {
+                //console.log(data);
+                var backgrd = "url(\"" + data.images[0].baseimageurl + "\");";
+                favBackgrd.push(backgrd);
+                console.log("premier While");
+            });
+            y++;
+        }
+    }).then(function() {
+        var z = 0;
+        while (z < idLength) {
+            var element = ids[0][z];
+            console.log("deuxieme While");
+            var elementChild = $(element).children();
+            console.log(z);
+            console.log(favBackgrd[z]);
+            $(elementChild[0])[0].style.backgroundImage = favBackgrd[z];
+            console.log($(elementChild[0])[0].style.backgroundImage);
+            z++;
+        }
     });
-    y++;
-}
+};
 
-console.log(favBackgrd);
-
-var z = 0;
-while (z < idLength) {
-    var element = ids[0][z];
-    //console.log($(element));
-    var elementChild = $(element).children();
-    console.log(z);
-    //console.log(backgrd);
-    $(elementChild[0]).css("background-image", favBackgrd[z]);
-    //console.log($(elementChild[0]));
-    z++;
-}
-
-
+getBkgnd();
 
 // ids[0].each(function(index) {
 //     //console.log(ids[0][index].id);
